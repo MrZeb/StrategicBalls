2c36902a18d3bab0bfb8350ea9aca5b446351487
Jonatan Rhodin <jonatan.rhodin@gmail.com>
2014-01-08 14:59:10 +0100

    Implementing some kind of test for sending move messages

--- src/se/footballaddicts/strategicballs/multiplayer/Move.java
+++ src/se/footballaddicts/strategicballs/multiplayer/Move.java
@@ -1,14 +1,42 @@
 package se.footballaddicts.strategicballs.multiplayer;
 
+import java.util.Arrays;
+
 public class Move
 {
-    public enum MoveType { PLAYER, BALL };
+    public enum MoveType { 
+        PLAYER(0), BALL(1);
+        
+        public int id;
+        
+        MoveType( int id )
+        {
+            this.id = id;
+        }
+        
+        public static MoveType getMoveTypeFromId( int id )
+        {
+            if ( id == 0 )
+            {
+                return PLAYER;
+            }
+            else if ( id == 1 )
+            {
+                return BALL;
+            }
+            else
+            {
+                return PLAYER;
+            }
+        }
+        
+    };
     
     private MoveType moveType;
-    private int[][] from;
-    private int[][] to;
+    private int[] from;
+    private int[] to;
     
-    public Move( MoveType moveType, int[][] from, int[][] to )
+    public Move( MoveType moveType, int[] from, int[] to )
     {
         this.moveType = moveType;
         this.from = from;
@@ -20,14 +48,44 @@ public class Move
         return moveType;
     }
 
-    public int[][] getFrom()
+    public int[] getFrom()
     {
         return from;
     }
 
-    public int[][] getTo()
+    public int[] getTo()
     {
         return to;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + Arrays.hashCode( from );
+        result = prime * result + ((moveType == null) ? 0 : moveType.hashCode());
+        result = prime * result + Arrays.hashCode( to );
+        return result;
+    }
+
+    @Override
+    public boolean equals( Object obj )
+    {
+        if( this == obj )
+            return true;
+        if( obj == null )
+            return false;
+        if( getClass() != obj.getClass() )
+            return false;
+        Move other = (Move) obj;
+        if( !Arrays.equals( from, other.from ) )
+            return false;
+        if( moveType != other.moveType )
+            return false;
+        if( !Arrays.equals( to, other.to ) )
+            return false;
+        return true;
     }
     
     
